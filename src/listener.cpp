d329d4fc6ee5c8f98e7ebaa3ef1a492011792756
Cem Asma <dev.cemas@gmail.com>
2019-11-02 23:42:44 +0300

    random variable and split added

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,9 +1,30 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  //ROS_INFO("I heard: [%s]", msg->data.c_str());
+
+  vector<string> seperated;
+  string message = msg->data;
+  stringstream ss(message);
+  string tok;
+
+  while(getline(ss, tok, ' ')) {
+    seperated.push_back(tok);
+  }
+
+  int deger = stoi(seperated[4].c_str());
+  float sonuc = atof(seperated[6].c_str());
+
+  ROS_INFO("%s %s %s %s %d %s %f", seperated[0].c_str(), seperated[1].c_str(), seperated[2].c_str(), seperated[3].c_str(), deger, seperated[5].c_str(), sonuc);
 }
 
 int main(int argc, char **argv)
--- src/talker.cpp
+++ src/talker.cpp
@@ -1,7 +1,8 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
+#include <bits/stdc++.h>
 #include <sstream>
+#include <random>
 
 int main(int argc, char **argv)
 {
@@ -12,13 +13,23 @@ int main(int argc, char **argv)
 
   ros::Rate loop_rate(10);
 
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution(INT_MIN, INT_MAX);
+  std::uniform_real_distribution<float> distribution2(-9999.0, 1000.0);
+ 
   int count = 0;
+  int rastgele = 0;
+  float toplam = 0.0;
   while (ros::ok())
   {
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "Merhaba Cem " << count;
+
+    rastgele = distribution(generator);
+    toplam = distribution2(generator);
+
+    ss << "merhaba yapbenzet " << count << " deger " << rastgele << " sonuc " << toplam ;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
